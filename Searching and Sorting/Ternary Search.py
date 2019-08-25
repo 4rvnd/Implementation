4fde1bb5bfe56ff447f6f7ecf31d9c304db71a06
Aravind <37099536+sarahgrbg@users.noreply.github.com>
2019-08-25 20:32:07 +0530

    last commit for the day

--- Searching and Sorting/Ternary Search.py
+++ Searching and Sorting/Ternary Search.py
@@ -1,23 +1,24 @@
 def ternary_search(arr, first, last, key):
-    while(first>=last):
-        mid1 = first + (last-first)//3
-        mid2 = last - (last-first)//3
+    if(first>=last):
+        mid1 = first + ((last-first)//3)
+        mid2 = last - ((last-first)//3)
 
         if(key == arr[mid1]):
             return mid1
         elif(key == arr[mid2]):
             return mid2
-        elif(key < mid1):
+        elif(key < arr[mid1]):
             return ternary_search(arr, first, mid1-1, key)
-        elif(key > mid2):
+        elif(key > arr[mid2]):
             return ternary_search(arr, mid2+1, last, key)
         else:
             return ternary_search(arr, mid1+1, mid2-1, key)
-    return -1
+    else:
+        return -1
 
 
 arr = [10, 20, 30, 40, 50]
-key = 50
+key = 20
 
 result = ternary_search(arr, 0, len(arr), key)
 
