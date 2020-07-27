7a260440616e3a346c34ae12159ceed57a3fb987
Aravind <aravind17mahadevan@gmail.com>
2020-07-27 20:42:33 +0530

    dsa

--- Searching/cpp/ternarySearch.cpp
+++ Searching/cpp/ternarySearch.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define loop(i, a, b) for(int i = a; i < b; i++)
+#define autoLoop(a, n) for(auto& a : n)
+#define sze(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
 void insertArray(vector<int> &nums) {
     int size, num;
