fd3dddb5b6d2a03bfdcfbf4681fec070d0bb635e
Aravind <aravind17mahadevan@gmail.com>
2020-07-27 18:49:31 +0530

    ternary

---
+++ Searching and Sorting/cpp/ternarySearch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertArray(vector<int> &nums) {
+    int size, num;
+    cin >> size;
+    for (int i=0; i<size; i++) {
+        cin >> num;
+        nums.push_back(num);
+    }
+}
+
+int ternarySearch(vector<int> &nums, int first, int last, int findNum) {
+    if (first <= last) {
+        int mid1 = first + (last-first)/3, mid2 = last - (last-first)/3;
+        if (findNum == nums[mid1])
+            return mid1;
+        else if(findNum == nums[mid2]) 
+            return mid2;
+        else if (findNum < nums[mid1]) {
+            return ternarySearch(nums, 0, mid1-1, findNum);
+        }
+        else if (findNum > nums[mid1] && findNum < nums[mid2]) {
+            return ternarySearch(nums, mid1+1, mid2-1, findNum);
+        }
+        else if (findNum > nums[mid2]){
+            return ternarySearch(nums, mid2+1, nums.size(), findNum);
+        }
+    }
+    else
+        return -1;
+}
+
+main() {
+    vector <int> nums;
+    int findNum;
+    insertArray(nums);
+    cin >> findNum;
+    int first = 0, last = nums.size();
+    int ret = ternarySearch(nums, first, last, findNum);
+    if (ret == -1) 
+        cout << "not found";
+    else
+        cout << "element found at " << ret << " position";
+}
